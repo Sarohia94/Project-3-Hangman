102dba1bf4bea4489d99d1df3c71c9b721215dbb
Sarohia94 <sarohia94@gmail.com>
2023-01-04 17:53:56 +0000

    Create get_random_word function to get random word from words.py

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
+import random
+from words import word_list
+
+print(word_list)  # check
+
+
+def get_random_word():
+    """
+    Gets a random word from words_list array from the word.py file.
+    """
+    word = random.choice(word_list)
+    return word.upper()
+
+
+print(get_random_word())  # check
+
 HANGMAN = ['''
    +---+
        |
