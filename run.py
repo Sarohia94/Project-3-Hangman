5a16aad1574ed9f0dc4c6c2d0ddea8603d73ce7c
Sarohia94 <sarohia94@gmail.com>
2023-01-08 16:55:51 +0000

    Add four functions to welcome user, get user name, present menu and learn how to play

--- run.py
+++ run.py
@@ -4,6 +4,8 @@ import random
 from words import word_list
 from hangman import HANGMAN
 
+NAME = ""
+
 
 def get_random_word():
     """
@@ -11,6 +13,111 @@ def get_random_word():
     """
     word = random.choice(word_list)
     return word.upper()
+
+
+def start_game():
+    """
+    Welcomes user and introduces game
+    """
+    print("""
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/                       
+    \n""")
+    print("Let's play hangman!\n")
+
+
+def player_name():
+    """
+    Requests users name.
+    This will be used when user is either hanged or guesses the full word.
+    """
+    global NAME
+    while True:
+        NAME = input("What is your name?\n").upper()
+        if NAME.isalpha():
+            break
+        os.system("clear")
+        start_game()
+        print("Please enter a valid name using letters.")
+    os.system("clear")
+    print(f"Welcome {NAME}!")
+    return NAME
+
+
+def menu():
+    """
+    Requests user to choose from menu.
+    Option to play game or learn how to play.
+    """
+    print(f"""{NAME} would you like to:\n
+          1. Start game\n
+          2. Learn how to play?\n""")
+    print("Please enter 1 to start game or 2 to learn how to play.\n")
+    choice_made = False
+    while choice_made is False:
+        menu_choice = input("Number: ")
+        try:
+            if menu_choice == "1":
+                return menu_choice
+                choice_made = True
+            if menu_choice == "2":
+                return menu_choice
+                choice_made = True
+            else:
+                os.system("clear")
+                start_game()
+                raise ValueError("Please enter 1 to start game" +
+                                 " or 2 to learn how to play.\n")
+        except ValueError as e:
+            print(f"\nInvalid data:\n{e}")
+
+
+def how_to_play():
+    """
+    Instructions for user unfamiliar to the game on how to play.
+    """
+    os.system("clear")
+    print("""
+      Hangman is a word guessing game.\n
+      The object of the game is to figure out the unknown word by guessing
+      letters.\n
+      The length of the word will be provided and it will be marked by
+      underscores for each letter.\n
+      You will be prompted to guess a single letter from the alphabet.\n
+      If the letter is in the unknown word it will display by replacing the
+      underscores.\n
+      If the guessed letter is not in the unknown word you will lose a try.\n
+      This will correspond to a person on the gallow being drawn,
+      one part for each incorrect letter guessed.\n
+      As such you will have 6 tries to guess before you are hanged and
+      lose the game!\n
+      i.e. in the order: head, body, left arm, right arm, left leg, right leg.
+      """)
+    print(f"Good luck {NAME}! Guess the word and win the game!\n")
+    print("Please enter 1 to start game or 2 to return to menu.\n")
+    choice_made = False
+    while choice_made is False:
+        menu_choice = input("Number: ")
+        try:
+            if menu_choice == "1":
+                start_game()
+                choice_made = True
+            if menu_choice == "2":
+                menu()
+                choice_made = True
+            else:
+                os.system("clear")
+                start_game()
+                raise ValueError("Please enter 1 to start game" +
+                                 " or 2 to return to menu.\n""")
+        except ValueError as e:
+            print(f"\nInvalid data: {e}")
 
 
 def display_game(incorrect_letters, correct_letters, word):
@@ -36,8 +143,8 @@ def display_game(incorrect_letters, correct_letters, word):
         guess = input("\nPlease guess a letter:").upper()
         if tries == 0:
             game_over = True
-            print("You've been hanged!")
-            print(f"\nThe word was {word}")
+            print("You've been hanged!\n")
+            print(f"The word was {word}\n")
         else:
             if len(word) == len(correct_letters):
                 game_over = True
@@ -87,6 +194,16 @@ def main():
     """
     Run all program functions
     """
+    start_game()
+    player_name()
+    menu_choice = menu()
+    if menu_choice == "1":
+        display_game()
+    elif menu_choice == "2":
+        how_to_play()
+    else:
+        return
+
     incorrect_letters = ""
     correct_letters = ""
     word = get_random_word()
