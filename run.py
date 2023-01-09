191f85e3bdc3cd5c91598fda34067bceac6753fd
Sarohia94 <sarohia94@gmail.com>
2023-01-09 22:36:10 +0000

    Add further global variables, tidy code and add print statements to check bugs

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-# All code or resources used are credited in the README file.
+# All code and resources used are credited in the README file.
 import os
 import random
 from words import word_list
@@ -8,14 +8,18 @@ NAME = ""
 TRIES = 6
 INCORRECT = ""
 CORRECT = ""
+WORD = ""
+GUESS_WORD = ""
 
 
 def get_random_word():
     """
     Gets a random word from words_list array from the word.py file.
     """
-    word = random.choice(word_list)
-    return word.upper()
+    global WORD
+
+    WORD = random.choice(word_list)
+    return WORD.upper()
 
 
 def start_game():
@@ -23,15 +27,16 @@ def start_game():
     Welcomes user and introduces game
     """
     print("""
- _                                             
-| |                                            
-| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+ _
+| |
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __
 | '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
 | | | | (_| | | | | (_| | | | | | | (_| | | | |
 |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
-                    __/ |                      
-                   |___/                       
+                    __/ |
+                   |___/
     \n""")
+
     print("Let's play hangman!\n")
 
 
@@ -67,11 +72,11 @@ def menu():
         menu_choice = input("Number: ")
         try:
             if menu_choice == "1":
-                return menu_choice
                 choice_made = True
-            if menu_choice == "2":
                 return menu_choice
+            elif menu_choice == "2":
                 choice_made = True
+                return menu_choice
             else:
                 os.system("clear")
                 start_game()
@@ -90,11 +95,7 @@ def how_to_play():
       Hangman is a word guessing game.\n
       The object of the game is to figure out the unknown word by guessing
       letters.\n
-      The length of the word will be provided and it will be marked by
-      underscores for each letter.\n
-      You will be prompted to guess a single letter from the alphabet.\n
-      If the letter is in the unknown word it will display by replacing the
-      underscores.\n
+      If the letter is in the unknown word it will display.\n
       If the guessed letter is not in the unknown word you will lose a try.\n
       This will correspond to a person on the gallow being drawn,
       one part for each incorrect letter guessed.\n
@@ -102,8 +103,7 @@ def how_to_play():
       lose the game!\n
       i.e. in the order: head, body, left arm, right arm, left leg, right leg.
       """)
-    print(f"Good luck {NAME}! Guess the word and win the game!\n")
-    print("Please enter 1 to play game or 2 to return to menu.\n")
+    print("Please enter 1 to play game.\n")
     choice_made = False
     while choice_made is False:
         menu_choice = input("Number: ")
@@ -111,14 +111,10 @@ def how_to_play():
             if menu_choice == "1":
                 display_game()
                 choice_made = True
-            if menu_choice == "2":
-                menu()
-                choice_made = True
             else:
                 os.system("clear")
                 start_game()
-                raise ValueError("Please enter 1 to play game" +
-                                 " or 2 to return to menu.\n""")
+                raise ValueError("Please enter 1 to play game.\n")
         except ValueError as e:
             print(f"\nInvalid data: {e}")
 
@@ -131,26 +127,30 @@ def display_game():
     """
     os.system("clear")
     global TRIES
-    word = get_random_word()
-    guess_word = "_" * len(word)
+    global GUESS_WORD
 
-    print(f"You have {len(HANGMAN)-1} attempts to guess the word!")
+    WORD = get_random_word()
+    GUESS_WORD = "_" * len(WORD)
 
-    print(f"\nThere are {len(word)} letters in this word")
+    print(f"Good luck {NAME}! Guess the word and win the game!\n")
 
-    print(word)  # check
+    print(f"You have {len(HANGMAN)-1} attempts to guess the word.")
+
+    print(f"\nThere are {len(WORD)} letters in this word.")
+
+    print(WORD)  # check
 
     game_over = False
     while not game_over and TRIES > 0:
         print(HANGMAN[len(INCORRECT)])
-        print(f"\nWord: {guess_word}\n")
+        print(f"\nWord: {GUESS_WORD}\n")
         ask_for_input()
-        if TRIES == 0:
+        if len(HANGMAN)-1 == len(INCORRECT):
             game_over = True
             print("You've been hanged!\n")
-            print(f"The word was {word}\n")
+            print(f"The word was {WORD}\n")
         else:
-            if len(word) == len(CORRECT):
+            if len(WORD) == len(CORRECT):
                 game_over = True
                 print("Congratulations! You've guessed the word.")
 
@@ -162,10 +162,11 @@ def ask_for_input():
     the check_correct function is called to check if it is in the word.
     Otherwise if guess is invalid error messages are raised.
     """
+    global TRIES
     check = True
     guess = ""
     while check:
-        guess = input("\nPlease guess a letter:").upper()
+        guess = input("\nPlease guess a letter: ").upper()
         try:
             if len(guess) == 1 and guess.isalpha():
                 if guess in INCORRECT or CORRECT:
@@ -173,7 +174,6 @@ def ask_for_input():
                                      guess)
                 else:
                     check_correct(guess)
-                    check = False
             if len(guess) > 1:
                 raise ValueError(f"You have guessed {len(guess)} "
                                  + "characters. Please guess 1 letter")
@@ -181,6 +181,7 @@ def ask_for_input():
                 raise ValueError("Please guess a letter.")
         except ValueError as e:
             print(f"\nInvalid data: {e}")
+        check = False
         return guess
 
 
@@ -192,22 +193,28 @@ def check_correct(guess):
     """
     global INCORRECT
     global CORRECT
+    global GUESS_WORD
+    global TRIES
 
-    if guess in word:
-        os.system("clear")
+    print("line 204", GUESS_WORD)  # check
+    print("line 205", guess)  # check
+    print("line 206", WORD)  # check
+
+    if guess in WORD.upper():
+#        os.system("clear")
         print("\nYes!", guess, "is in the word!")
         CORRECT = CORRECT + guess
-        word_as_list = list(guess_word)
-        indices = [i for i, letter in enumerate(word)
+        word_as_list = list(GUESS_WORD)
+        indices = [i for i, letter in enumerate(WORD.upper())
                    if letter == guess]
         for index in indices:
             word_as_list[index] = guess
-            guess_word = "".join(word_as_list)
-        return guess
+            GUESS_WORD = "".join(word_as_list)
     else:
-        os.system("clear")
-        print(guess, "is not in the word\n")
+#        os.system("clear")
+        print(guess, "is not in the word.\n")
         INCORRECT = INCORRECT + guess
+        TRIES -= 1
         print(f"You have {TRIES} attempts remaining!")
         print('\nIncorrect guesses:', end=" ")
         for guess in INCORRECT:
@@ -222,7 +229,6 @@ def main():
     player_name()
     menu_choice = menu()
     if menu_choice == "1":
-        word = get_random_word()
         display_game()
     elif menu_choice == "2":
         os.system("clear")
